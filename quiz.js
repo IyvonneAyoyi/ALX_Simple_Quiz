21c8b1b188e825297f7916fcfb2ef3b987d02ecf
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-03 02:10:28 +0300

    Identify correct answer

--- quiz.js
+++ quiz.js
@@ -2,3 +2,6 @@ function checkAnswer() {
     let correctAnswer = "4";
 }
 
+// Retrieve answers
+let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+console.log(userAnswer)
